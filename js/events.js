3b6f1f45f0b3411c397f6c662c0fcd84fc6dde61
jom-hedges <jom_hedges@proton.me>
2025-03-27 20:26:25 +0800

    lol classic scratch

--- js/events.js
+++
@@ -1,6 +0,0 @@
-export const beginEvent = (startGame) => {
-  document.getElementById("runButton").addEventListener("click", function() {
-    startGame();
-    this.disabled = true;
-  })
-}
--- js/utils.js
+++
@@ -1,2 +0,0 @@
-export const getRandomColor = () =>
-  "#" + Array.from({ length: 6 }, () => "0123456789ABCDEF"[Math.floor(Math.random() * 16)]).join("");
